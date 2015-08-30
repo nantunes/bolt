9274b4e58dcfc7ba61ba31ab2c512fdf8ea2217a
Ross Riley <riley.ross@gmail.com>
2015-08-30 19:33:26 +0100

    fix the init of the repo

--- src/Storage/Field/Type/TemplateFieldsType.php
+++ src/Storage/Field/Type/TemplateFieldsType.php
@@ -50,7 +50,8 @@ class TemplateFieldsType extends FieldTypeBase
         $key = $this->mapping['fieldname'];
         $metadata = $this->buildMetadata($entity);
         
-        $repo = $this->repository($metadata);
+        $repoBuilder = $this->repository;
+        $repo = $repoBuilder($metadata);
         $templatefieldsEntity = $repo->create($value);
         
         $ct = new ContentType('templatefields', ['fields' => $metadata->getFieldMappings()]);
